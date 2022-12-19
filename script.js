ee15d6d784afb8f09f4213d4eec56c45733587e4
DylanMH <dylan98909@yahoo.com>
2022-12-18 20:01:34 -0600

    Initial Upload
    Fully Updated Personal Portfolio with full bootstrap classes!

---
+++ script.js
@@ -0,0 +1,9 @@
+function relocate_resume() {
+    var url = "https://docs.google.com/document/d/1Crv9FX98emd14OhnpAHAyt5cSAwPO1ur/edit"
+    window.open(url, "_blank")
+}
+
+function relocate_email() {
+    var url = "mailto:dylan98909@gmail.com"
+    window.open(url, "_blank")
+}
